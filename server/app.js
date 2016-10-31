3c425c94f3eae6873fd50437ebe5ca6970c09068
Pepsi33 <319853035@qq.com>
2016-10-31 22:11:27 +0800

    端口

--- server/app.js
+++ server/app.js
@@ -3,9 +3,8 @@ var express = require("express");
 var app = new express();
 
 var path = require("path");
-
-var staticpath = path.join(__dirname,"../static");
+var saticpath = path.join(__dirname,"../static");
 app.use(express.static(saticpath));
-app.listen(3000,function(){
-	console.log("open http://127.0.0.1:3000");
+app.listen(80,function(){
+	console.log("open http://127.0.0.1:80");
 })
