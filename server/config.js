f75f74be7a5acc6caa1b3687c8acd191d156ed81
Pepsi33 <319853035@qq.com>
2016-11-01 21:32:20 +0800

    ---提交token

---
+++ server/config.js
@@ -0,0 +1,16 @@
+/**
+ * 微信认证后的 app 信息
+ * 如果有需要可以将这些信息放到数据库中读取方便以更增加
+ * 注：测试时请替换您真实的数据
+ */
+module.exports = function(){
+	return {
+		//AppID(应用ID)
+		appid: 'wx23d705a48d9dc925
+',
+		//AppSecret(应用密钥)
+		appsecret: '0f3cb62870dd9b426a3c2da250beb79a',
+		//Token(令牌)
+		token: 'weixin'
+	};
+};
