c22cbe2d696500ccf954c49f8757f8ef37ae1f90
Pepsi33 <319853035@qq.com>
2016-11-01 21:32:05 +0800

    ---提交token

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ var express = require("express");
 
 var app = new express();
 
+//加载token模块
+require("./token")(app)
+
 var path = require("path");
-var saticpath = path.join(__dirname,"../static");
+var saticpath = path.join(__dirname,"../");
 app.use(express.static(saticpath));
 app.listen(80,function(){
 	console.log("open http://127.0.0.1:80");
