1a87df06ee9a37f6f5d3a000c6c9ff1aa51ffdd1
Pepsi33 <319853035@qq.com>
2016-11-01 21:38:08 +0800

    ---comon

--- server/config.js
+++ server/config.js
@@ -6,8 +6,7 @@
 module.exports = function(){
 	return {
 		//AppID(应用ID)
-		appid: 'wx23d705a48d9dc925
-',
+		appid: 'wx23d705a48d9dc925',
 		//AppSecret(应用密钥)
 		appsecret: '0f3cb62870dd9b426a3c2da250beb79a',
 		//Token(令牌)
