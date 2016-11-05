402ef8cc6af5288f9164a738e6725a4a58812a9e
Pepsi33 <319853035@qq.com>
2016-11-05 15:07:29 +0800

    地理位置功能

--- static/test.js
+++ static/test.js
@@ -20,7 +20,9 @@ $(function(){
             'onMenuShareQQ',
             'onMenuShareWeibo',
             //获取用户网络状态权限
-            'onGetNetworkType'
+            'onGetNetworkType',
+            //获取地理位置
+            'onGetLocation'
 		].join(",")
 	},function(resp){
 		wx.config({
@@ -54,3 +56,16 @@ $("#getNetworkType").on("click",function(){
 		});
 	});
 
+
+$().on('click',function(){
+	wx.getLocation({
+	    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+	    success: function (res) {
+	        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+	        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+	        var speed = res.speed; // 速度，以米/每秒计
+	        var accuracy = res.accuracy; // 位置精度
+	        alert("维度"+latitude+"精度"+longitude+"速度"+speed+"位置精度"+accuracy)
+	    }
+	});
+})
