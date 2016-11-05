d91a3d72691189a8893f9aa556547c76035340cf
Pepsi33 <319853035@qq.com>
2016-11-05 14:39:56 +0800

    分享测试

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 var express = require("express");
+//post 请求处理模块
+var bodyparser = require("body-parser");
 
 var app = new express();
+//处理post请求 将请求数据封装为json
+app.use(bodyparser.urlencoded({
+	extended: true
+}));
 
 //加载token模块
-require("./token.js")(app)
+require("./token.js")(app);
+require("./jssdk.js")(app);
 
 var path = require("path");
 var saticpath = path.join(__dirname,"../static");
