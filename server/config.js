a4872355eed0cb1a6dc115764d3f8ecd95774114
Pepsi33 <319853035@qq.com>
2016-11-13 21:58:42 +0800

    找回app

--- server/config.js
+++ server/config.js
@@ -6,9 +6,9 @@
 module.exports = function(){
 	return {
 		//AppID(应用ID)
-		appid: '',
+		appid: 'wx23d705a48d9dc925',
 		//AppSecret(应用密钥)
-		appsecret: '',
+		appsecret: '0f3cb62870dd9b426a3c2da250beb79a',
 		//Token(令牌)
 		token: 'weixin'
 	};
